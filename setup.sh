f28b9ff22ddf6df353e23a968a226dc9a330f9de
Yi Chen <happyyi008@gmail.com>
2017-12-29 15:42:12 -0500

    add file check for setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 go install github.com/happyyi008/remember
-mv $GOBIN/remember /usr/local/bin/rmb
 
-
+if [ -f $GOBIN/remember ]; then
+    sudo mv $GOBIN/remember /usr/local/bin/rmb
+else
+    sudo mv $GOPATH/bin/remember /usr/local/bin/rmb
+fi
